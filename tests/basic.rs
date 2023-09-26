5b05a2673720d2c02ad545d1bdb69d23c91558fe
Phantomical <phantom@lynches.ca>
2023-09-25 17:32:15 -0700

    Make variants without explicit values properly follow C enum rules
    In C, enum variants without an explicit value take the value $prev + 1,
    or 0, if they are the first variant. The constants generated by this
    crate are meant to follow the same convention.
    
    Before this commit, due to a bug in the macro, they don't follow that
    convention. What happens instead is that all variants without assigned
    values have the value 0, unless they directly follow a variant with an
    assigned value, in which case they take the same value as the previous
    variant. This is _wrong_. The fix here is to explicitly pass a default
    value through and set that to either 0 (for the first field) or
    Self::$prev + 1 (for all other fields).
    
    I have also included test cases that verify that the values of enum
    variants are as expected.

--- tests/basic.rs
+++ tests/basic.rs
@@ -2,19 +2,6 @@ use c_enum::*;
 
 c_enum! {
     #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-    pub enum Hardware : u64 {
-        /// Doc comments
-        CPU_CYCLES,
-        INSTRUCTIONS = 2,
-        CACHE_REFERENCES,
-        CACHE_MISSES,
-        BRANCH_INSTRUCTIONS = 5,
-        Lowercase,
-    }
-}
-
-c_enum! {
-    #[derive(Clone, Copy, PartialEq, Eq, Hash)]
     pub enum Software : u64 {
         /// Doc comments
         CPU_CYCLES,
@@ -35,6 +22,12 @@ c_enum! {
 }
 
 #[test]
+fn increment_after_assign() {
+    assert_eq!(Software::Lowercase.0, 6);
+    assert_ne!(Software::Lowercase, Software::BRANCH_INSTRUCTIONS);
+}
+
+#[test]
 fn duplicates_are_equal() {
     assert_eq!(Duplicates::ITEM1, Duplicates::ITEM2);
 }
@@ -50,4 +43,22 @@ fn variant_label_duplicate() {
     // the one whose label is used.
     assert_eq!(Duplicates::ITEM1.variant_label(), Some("ITEM1"));
     assert_eq!(Duplicates::ITEM2.variant_label(), Some("ITEM1"));
+}
+
+#[test]
+fn variant_label_overlap_assigned() {
+    c_enum! {
+        #[derive(Clone, Copy, PartialEq, Eq, Hash)]
+        enum Overlap : u32 {
+            A1 = 2,
+            A2,
+            A3,
+            B1 = 2,
+            B2,
+            B3
+        }
+    }
+
+    assert_eq!(Overlap::A1, Overlap::B1);
+    assert_eq!(Overlap::A3, Overlap::B3);
 }
